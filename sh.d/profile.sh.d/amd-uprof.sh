3dc412137b8aa393fff967a3501fca57ec617ff3
YU Zhejian <Zhejian.23@intl.zju.edu.cn>
2024-12-09 20:00:15 +0800

    AMD profiler (uProf) added.

---
+++ sh.d/profile.sh.d/amd-uprof.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2317
+AMD_COMPILER_PATH="/opt/AMD/aocc-compiler-5.0.0/"
+AMD_UPROF_PATH="/opt/AMDuProf_5.0-1479/"
+AMD_AOCL_PATH="/opt/AMD/aocl/aocl-linux-aocc-5.0.0/aocc/"
+set +ue
+. "${AMD_COMPILER_PATH}"/setenv_AOCC.sh
+. "${AMD_AOCL_PATH}"/amd-libs.cfg
+set -ue
+
+PROFILE_DIR=opt/build_profile_amd_uprof
+mkdir -p "${PROFILE_DIR}"
+
+env -C "${PROFILE_DIR}" cmake \
+    -DCMAKE_C_COMPILER="${AMD_COMPILER_PATH}"/bin/clang \
+    -DCMAKE_CXX_COMPILER="${AMD_COMPILER_PATH}"/bin/clang++ \
+    -DCEU_CM_SHOULD_USE_NATIVE=ON \
+    -DCEU_CM_SHOULD_ENABLE_TEST=OFF \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DUSE_RANDOM_GENERATOR=STL \
+    -G Ninja "$(pwd)"
+
+env -C "${PROFILE_DIR}" ninja -j120
+
+"${PROFILE_DIR}"/art_modern --version
+ldd "${PROFILE_DIR}"/art_modern
+rm -fr "${PROFILE_DIR}"/uprof_results
+"${AMD_UPROF_PATH}"/bin/AMDuProfCLI \
+    collect \
+    --config hotspots \
+    --output-dir "${PROFILE_DIR}"/uprof_results \
+    "${PROFILE_DIR}"/art_modern \
+    --qual_file_1 data/Illumina_profiles/HiSeq2500L125R1.txt \
+    --qual_file_2 data/Illumina_profiles/HiSeq2500L125R2.txt \
+    --i-file data/raw_data/ce11_chr1.fa \
+    --read_len 125 \
+    --mode wgs \
+    --lc pe \
+    --i-parser memory \
+    --i-fcov 10 \
+    --parallel 0 \
+    --ins_rate_1 0.1 \
+    --del_rate_1 0.1 \
+    --pe_frag_dist_std_dev 20 \
+    --pe_frag_dist_mean 500 \
+    --o-sam "${PROFILE_DIR}"/test_small_se_wgs_memory.bam \
+    --o-sam-write_bam \
+    --o-hl_sam "${PROFILE_DIR}"/test_small_se_wgs_memory.hl.sam \
+    --o-fastq "${PROFILE_DIR}"/test_small_se_wgs_memory.fastq \
+    --o-pwa "${PROFILE_DIR}"/test_small_se_wgs_memory.pwa
+"${AMD_UPROF_PATH}"/bin/AMDuProf \
+    --session "${PROFILE_DIR}"/uprof_results/* \
+    --bin-path "${PROFILE_DIR}" --src-path "$(pwd)"
