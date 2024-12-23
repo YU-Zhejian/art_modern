d969c5e81af5f05f2fb10b25fa5ef2aa83518a33
YU Zhejian <Zhejian.23@intl.zju.edu.cn>
2024-12-23 14:10:47 +0800

    Small bugs fixed

--- explore/benchmark_other_simulators/plot_depth.R
+++ explore/benchmark_other_simulators/plot_depth.R
@@ -1,6 +1,18 @@
 library(dplyr)
 library(ggplot2)
 
+replacement_list <- list(
+  "pirs" = "pIRS",
+  "dwgsim" = "DWGSIM",
+  "wgsim" = "wgsim",
+  "art" = "Original ART",
+  "art_modern" = "art_modern",
+  "real_1" = "Read 1",
+  "real_2" = "Read 2",
+  "real" = "Input Data"
+)
+
+
 all_depth <- data.frame()
 
 for (software in c("art_modern", "art", "pirs", "wgsim", "dwgsim")){
@@ -11,6 +23,11 @@ for (software in c("art_modern", "art", "pirs", "wgsim", "dwgsim")){
     all_depth <- rbind(all_depth, depth_table)
   }
 }
+all_depth <- readr::read_tsv("./all_depth.tsv") %>%
+  dplyr::mutate(
+    software = sapply(software, function(x)
+      replacement_list[[x]])
+  ) 
 
 p <- ggplot(all_depth) +
   geom_line(aes(x = pos, y = depth, color = software)) +
@@ -29,7 +46,9 @@ p <- ggplot(all_depth) +
 ggsave("fig/depth_last1k.pdf", p, width = 8, height = 4)
 
 p <- ggplot(all_depth) +
-  geom_violin(aes(y = software, x = depth, fill=rlen)) +
-  geom_vline(xintercept = 100) +
-  theme_bw()
-ggsave("fig/depth_box.pdf", p, width = 5, height = 4)
+  geom_violin(aes(y = software, x = depth, fill=as.factor(rlen))) +
+  geom_vline(xintercept = 100, color="red") +
+  scale_fill_discrete("Read length") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+ggsave("fig/depth_box.pdf", p, width = 7, height = 2)
