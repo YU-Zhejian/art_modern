b5322f22099b91705d61a50cc547b60858a3a603
YU Zhejian <yuzj25@seas.upenn.edu>
2025-09-13 16:13:28 -0400

    Extensive revision was done to suppress lintian complains.

--- sh.d/deb.sh
+++ sh.d/deb.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2086
+# TODO: Supress -Wdate-time
+# TODO: FIX: exit-in-shared-library
 set -ue
-export DEBUILD_LINTIAN_OPTS="-i -I --show-overrides --pedantic"
 
 SRC_DIR="$(dirname "$(readlink -f "${0}")")/../"
 cd "${SRC_DIR}" || exit 1
@@ -12,23 +13,38 @@ echo "DEB: Building for version ${PACKAGE_VERSION}"
 
 # Generate orig tarball from git files
 echo "DEB: Generate orig tarball from git files"
-git ls-files | grep -v '^debian/' > opt/build_deb/git-files.txt
-mkdir -p opt/build_deb/art-modern-${PACKAGE_VERSION}
+git ls-files |
+    grep -v '^debian/' |
+    grep -v '^.github/' |
+    grep -v '^.vscode/' |
+    grep -v '^bioconda/' |
+    grep -v '^chroot/' |
+    grep -v '^deps/labw_slim_htslib/' |
+    grep -v '^explore/' |
+    grep -v '^env/' \
+        >opt/build_deb/git-files.txt
+export BUILD_DIR="opt/build_deb/art-modern-${PACKAGE_VERSION}+dfsg"
+mkdir -p "${BUILD_DIR}"
 while read -r line; do
     if [ -f "${line}" ]; then
-        install -D "${line}" "opt/build_deb/art-modern-${PACKAGE_VERSION}/${line}"
+        install -D "${line}" "${BUILD_DIR}/${line}"
     fi
-done < opt/build_deb/git-files.txt
-env -C opt/build_deb/ tar -czf art-modern_${PACKAGE_VERSION}.orig.tar.gz art-modern-${PACKAGE_VERSION}
+done <opt/build_deb/git-files.txt
+env -C opt/build_deb/ tar -czf "art-modern_${PACKAGE_VERSION}+dfsg.orig.tar.gz" "art-modern-${PACKAGE_VERSION}+dfsg"
 
 # Copy debian files
 echo "DEB: Copy debian files"
-cp -r debian opt/build_deb/art-modern-${PACKAGE_VERSION}/debian
+cp -r debian "${BUILD_DIR}/debian"
 
 # Build the deb
 echo "DEB: Build the deb"
-env -C opt/build_deb/art-modern-${PACKAGE_VERSION} debuild -us -uc \
-    > >( sed 's/^/DEBUILD-INFO: /' | tee opt/build_deb/debuild-info.log) \
-    2> >( sed 's/^/DEBUILD-ERR: /' | tee opt/build_deb/debuild-err.log >&2 )
-env -C opt/build_deb/art-modern-${PACKAGE_VERSION} debuild -- clean
-# TODO: Supress -Wdate-time
+env -C "${BUILD_DIR}" debuild -us -uc \
+    > >(sed --unbuffered 's/^/DEBUILD-INFO: /' | tee opt/build_deb/debuild-info.log) \
+    2> >(sed --unbuffered 's/^/DEBUILD-ERR: /' | tee opt/build_deb/debuild-err.log >&2)
+env -C "${BUILD_DIR}" debuild -- clean &>/dev/null
+env -C "${BUILD_DIR}" debc 2>&1 | sed 's/^/DEBCHECK: /' | tee opt/build_deb/debcheck.log
+env -C opt/build_deb/ \
+    lintian art-modern_${PACKAGE_VERSION}+dfsg-1_amd64.changes \
+    -EIi --pedantic --tag-display-limit 0 2>&1 |
+    sed 's/^/LINTIAN: /' |
+    tee opt/build_deb/lintian.log
