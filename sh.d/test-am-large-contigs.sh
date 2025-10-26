101fcc3756fdbc63db0e6d4895a95dab9959ed46
YU Zhejian <yuzj25@seas.upenn.edu>
2025-10-25 23:28:20 -0400

    Code reformatted

--- sh.d/test-am-large-contigs.sh
+++ sh.d/test-am-large-contigs.sh
@@ -6,19 +6,18 @@ SHDIR="$(readlink -f "$(dirname "${0}")")"
 cd "${SHDIR}/../"
 make -C "${SHDIR}"/test-am-large-contigs.d/
 
-
-    opt/build_release_install/bin/art_modern \
-        --builtin_qual_file HiSeq2500_125bp \
-        --i-file "${SHDIR}"/test-am-large-contigs.d/large_contigs.fa.gz \
-        --i-type fasta \
-        --read_len 125 \
-        --mode wgs \
-        --lc se \
-        --i-parser htslib \
-        --i-fcov 20 \
-        --parallel 0 \
-        --o-sam /dev/null \
-        --o-sam-num_threads 4 \
-        --o-sam-compress_level u \
-        --o-sam-write_bam \
-        --o-fastq /dev/null
+opt/build_release_install/bin/art_modern \
+    --builtin_qual_file HiSeq2500_125bp \
+    --i-file "${SHDIR}"/test-am-large-contigs.d/large_contigs.fa.gz \
+    --i-type fasta \
+    --read_len 125 \
+    --mode wgs \
+    --lc se \
+    --i-parser htslib \
+    --i-fcov 20 \
+    --parallel 0 \
+    --o-sam /dev/null \
+    --o-sam-num_threads 4 \
+    --o-sam-compress_level u \
+    --o-sam-write_bam \
+    --o-fastq /dev/null
