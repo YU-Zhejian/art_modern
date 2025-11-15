310534c5cf766a67a25f9505755dac7dfb7c1925
YU Zhejian <yuzj25@seas.upenn.edu>
2025-11-15 18:39:14 -0500

    Missing copyright fixed

--- src/libam_support/seq/qual_to_str.h
+++ src/libam_support/seq/qual_to_str.h
@@ -1,6 +1,16 @@
-//
-// Created by yuzj on 11/1/25.
-//
+/**
+ * Copyright 2025 YU Zhejian <yuzj25@seas.upenn.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ **/
 
 #ifndef ART_MODERN_LIBAM_SUPPORT_QUAL_TO_STR_H
 #define ART_MODERN_LIBAM_SUPPORT_QUAL_TO_STR_H
